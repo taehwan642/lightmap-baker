caf2edbb5bbc4a4151b49866fd64526dd5bb5804
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2024-04-08 21:47:34 +0900

    [Data] initial commit

---
+++ Data/DataManager.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+namespace LightmapBaker
+{
+namespace Data
+{
+    class DataManager
+    {
+    private:
+    public:
+        void Save(std::string path);
+        void Load(std::string path);
+    };
+}
+}
