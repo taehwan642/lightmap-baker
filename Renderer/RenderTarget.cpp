3b9579de0d131c65e577830d7fe9e698b939d15f
Tipp7 <35156090+Rlaxogus123@users.noreply.github.com>
2024-04-20 21:51:51 +0900

    [Light] added frame buffer texture filter setting code

--- Renderer/RenderTarget.cpp
+++ Renderer/RenderTarget.cpp
@@ -15,6 +15,8 @@ void LightmapBaker::Renderer::RenderTarget::Initialize(glm::vec2 resolution)
 
     glBindTexture(GL_TEXTURE_2D, renderTexture);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, resolution.x, resolution.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTexture, 0);
 
     glBindRenderbuffer(GL_RENDERBUFFER, depthBuffer);
